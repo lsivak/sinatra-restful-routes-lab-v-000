11f322ce2786edb0391925a12b53ac6cec4b6f93
Lara Sanders <larasanders@gmail.com>
2018-01-07 22:18:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,10 +45,10 @@ class ApplicationController < Sinatra::Base
 
       redirect to "/recipes/#{@recipe.id}"
    end
-
-  post '/recipes/:id' do
-    erb :show
-  end
+  # 
+  # post '/recipes/:id' do
+  #   erb :show
+  # end
 
   delete '/recipes/:id/delete' do
     @recipe = Recipe.find_by_id(params[:id])

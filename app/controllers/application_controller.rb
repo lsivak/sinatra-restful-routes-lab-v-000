f3bd050d47fd369b646457d47b568b8a2091440d
Lara Sanders <larasanders@gmail.com>
2018-01-07 20:51:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
-    redirect to 'show'
+    erb :'/show'
   end
 
   get '/recipes' do
